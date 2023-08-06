9760389995ed34428763a773976f1b7ef3e6b232
Krish Bhurtel <alexandergarces987k@gmail.com>
2023-08-06 11:48:08 +0545

    overloading function completed

---
+++ overloadfunc.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std ;
+
+float floatnum(float a){  //function to return float 
+    return a ; 
+}
+
+int intnum(int a){
+    return a ; 
+}
+
+int main(){
+
+cout << "The float number is" << floatnum(2.4) << endl   ; 
+cout << "The int num is" <<  intnum(2) ;
+}
