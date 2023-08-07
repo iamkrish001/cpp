6c3ed9f9393b6e0a54fd2fff718c306543a5c49a
Krish Bhurtel <alexandergarces987k@gmail.com>
2023-08-07 11:42:47 +0545

    function series 2

---
+++ funcwithpara.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std ; 
+int Display(int x, int y){ // defining 
+cout << "The first num is" << x << endl; 
+cout << "The second num is" << y ;
+}
+
+int main (){
+
+Display(2,3) ; // function call with paramaters
+
+
+}
