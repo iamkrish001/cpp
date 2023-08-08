eccea07648755c4ed8b893aab8cac7bb515e0273
Krish Bhurtel <alexandergarces987k@gmail.com>
2023-08-08 11:22:30 +0545

    updated code

--- funcwithparaadd.cpp
+++ funcwithparaadd.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std ; 
-int AddTwo(int a, int b ){
-return  a+b   ; 
+int AddTwo(int a, int b ){   // created functions with paramters 
+return  a+b   ;   // returns the value of a+b , it goes to AddTwo function
 }
 int main (){ 
     int a,b ; 
 cout<< "Enter any two numbers"<< endl ;
 cin>> a >>  b ; 
-cout << " The sum is " <<  AddTwo(a,b) << endl ; 
+cout << " The sum is " <<  AddTwo(a,b) << endl ;   // the function is called here with entered parameters 
 
 }
