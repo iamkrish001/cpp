fe4cceece755a7e737144a1ee907119f604e28b4
Krish Bhurtel <alexandergarces987k@gmail.com>
2023-08-08 11:45:42 +0545

    array

---
+++ addarray.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std ; 
+
+int main (){
+    int marks[4] = {1,2,3} ; // last element of array is empty so I will add the value below
+
+cout << "Enter your last element of array" << endl ; 
+marks[3] = 10 ; 
+cin >> marks[3] ;
+for(i=1; i<=marks[4], i++){
+cout << "Printing the element of array" << marks[3] << endl; 
+}
+}
