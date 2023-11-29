4a944b81afdb2c23e4e83efc215a67090479e0b4
Krish Bhurtel <alexandergarces987k@gmail.com>
2023-11-29 19:49:10 +0545

    inline function done

---
+++ basicsofc++/inlinefunc.cpp
@@ -0,0 +1,26 @@
+// inline function 
+// it is used to solve overhead= stacks and control trf from function call to main func
+
+#include<iostream>
+using namespace std ;
+ 
+ inline void printname(string x){   
+    cout << x << endl ; 
+ }
+
+ int main (){
+    printname("My name is krish") ;  // copies the entire function code wherever it is called
+    printname("My name is krish") ;  // copies the entire function code wherever it is called
+    printname("My name is krish") ;  // copies the entire function code wherever it is called
+    printname("My name is krish") ;  // copies the entire function code wherever it is called
+    printname("My name is krish") ;  // copies the entire function code wherever it is called
+    printname("My name is krish") ;  // copies the entire function code wherever it is called
+    printname("My name is krish") ;  // copies the entire function code wherever it is called
+    printname("My name is krish") ;  // copies the entire function code wherever it is called
+    printname("My name is krish") ;  // copies the entire function code wherever it is called
+    printname("My name is krish") ;  // copies the entire function code wherever it is called
+    printname("My name is krish") ;  // copies the entire function code wherever it is called
+    printname("My name is krish") ;  // copies the entire function code wherever it is called
+    printname("My name is krish") ;  // copies the entire function code wherever it is called
+    printname("My name is krish") ;  // copies the entire function code wherever it is called
+ }
