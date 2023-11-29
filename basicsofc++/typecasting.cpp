cd68caa96f2026bbaf29a64f75023cf7fdac761f
Krish Bhurtel <alexandergarces987k@gmail.com>
2023-11-29 22:43:43 +0545

    typecasting done

---
+++ basicsofc++/typecasting.cpp
@@ -0,0 +1,12 @@
+// typecasting means converting one data type to another
+#include<iostream>
+using namespace std ;
+
+int main (){
+
+    float f = 3.595 ; 
+    char s = 'a' ; 
+
+    cout << " The integer value of f is :   " << int(f) << endl ; 
+    cout << " The integer value of s is :   " << int(s) << endl ; 
+}
